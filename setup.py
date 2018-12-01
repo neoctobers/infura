c98f236e98a0b16ef2520bcd5f8dae2df7db829d
neoctobers <neoctobers@gmail.com>
2018-12-01 17:04:50 +0800

    add install_requires

--- setup.py
+++ setup.py
@@ -19,5 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[],
+    install_requires=[
+        'requests_cache',
+    ],
 )
